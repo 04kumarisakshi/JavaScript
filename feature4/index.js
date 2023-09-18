320327a4e5000fdf15983d30424b885936068cf4
Kumari sakshi <117385471+04kumarisakshi@users.noreply.github.com>
2023-09-18 21:28:18 +0530

    index.js

--- feature4/index.js
+++ feature4/index.js
@@ -1,25 +1,23 @@
-// IMAGE SLIDES & CIRCLES ARRAYS, & COUNTER
+
 var imageSlides = document.getElementsByClassName('imageSlides');
 var circles = document.getElementsByClassName('circle');
 var leftArrow = document.getElementById('leftArrow');
 var rightArrow = document.getElementById('rightArrow');
 var counter = 0;
 
-// HIDE ALL IMAGES FUNCTION
+
 function hideImages() {
   for (var i = 0; i < imageSlides.length; i++) {
     imageSlides[i].classList.remove('visible');
   }
 }
 
-// REMOVE ALL DOTS FUNCTION
 function removeDots() {
   for (var i = 0; i < imageSlides.length; i++) {
     circles[i].classList.remove('dot');
   }
 }
 
-// SINGLE IMAGE LOOP/CIRCLES FUNCTION
 function imageLoop() {
   var currentImage = imageSlides[counter];
   var currentDot = circles[counter];
@@ -29,7 +27,7 @@ function imageLoop() {
   counter++;
 }
 
-// LEFT & RIGHT ARROW FUNCTION & CLICK EVENT LISTENERS
+// Arrow
 function arrowClick(e) {
   var target = e.target;
   if (target == leftArrow) {
@@ -66,7 +64,7 @@ leftArrow.addEventListener('click', arrowClick);
 rightArrow.addEventListener('click', arrowClick);
 
 
-// IMAGE SLIDE FUNCTION
+
 function slideshow() {
   if (counter < imageSlides.length) {
     imageLoop();
@@ -77,6 +75,5 @@ function slideshow() {
   }
 }
 
-// SHOW FIRST IMAGE, & THEN SET & CALL SLIDE INTERVAL
 setTimeout(slideshow, 1000);
 var imageSlideshowInterval = setInterval(slideshow, 10000);
